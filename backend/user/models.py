a50691b6e2e7c31a19a649f78c49870c573efa25
MIXISAMA <34473927+MIXISAMA@users.noreply.github.com>
2019-12-17 22:59:20 +0800

    添加两个触发器 现在在admin可以直接设置User密码 不做检验并hash保存

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,9 +1,8 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User as AuthUser
-
-
-
-# Create your models here.
+from rest_framework.authtoken.models import Token
 
 class User(AuthUser):
     nickname = models.CharField(max_length=30, verbose_name='昵称')
@@ -22,3 +21,16 @@ class User(AuthUser):
         my_group = Group.objects.create(name=self.nickname+"的小组")
         Membership.objects.create(group=my_group, user=self, permission='master')
         return my_group
+
+@receiver(pre_save, sender=User, dispatch_uid="创建之前要检查密码")
+def before_create_user(instance, **kwargs):
+    if instance.id:
+        return
+    if not instance.password:
+        return
+    instance.set_password(instance.password)
+
+@receiver(post_save, sender=User, dispatch_uid="创建之后要自动生成令牌")
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
