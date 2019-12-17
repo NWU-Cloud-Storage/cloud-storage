46831692bf89aa5b68468cc8f38fd82b1091c335
MIXISAMA <34473927+MIXISAMA@users.noreply.github.com>
2019-12-17 23:23:21 +0800

    添加 code转token功能

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from user.views.user import User
-
+from user.views.token import OAuthToken
 
 
 urlpatterns = [
     path("user/", User.as_view()),
-    path("user/<str:username>/", User.as_view())
+    path("user/<str:username>/", User.as_view()),
+    path("login/<str:code>/", OAuthToken.as_view())
 ]
