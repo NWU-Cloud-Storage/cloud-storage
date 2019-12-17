a1d4016801e62ea57c02a4afc8cecf36f307d2f0
MIXISAMA <34473927+MIXISAMA@users.noreply.github.com>
2019-12-17 14:37:14 +0800

    完善Storage模块

--- backend/storage/checker.py
+++ backend/storage/checker.py
@@ -11,18 +11,30 @@ from rest_framework.exceptions import PermissionDenied
 from storage.models import Catalogue
 
 def check_exist_catalogue(cata_id):
+    '''
+    检查应存在该目录。
+    '''
     catalogue = get_object_or_404(Catalogue, id=cata_id)
     return catalogue
 
 def check_exist_catalogues(cata_ids):
+    '''
+    检查存在这些目录。
+    '''
     catalogues = get_list_or_404(Catalogue, id__in=cata_ids)
     return catalogues
 
 def check_not_root(catalogue):
+    '''
+    检查目录非根。
+    '''
     if catalogue.is_root_node():
         raise ParseError()
 
 def check_are_siblings(catas):
+    '''
+    检查这些目录是兄弟结点。
+    '''
     tmp_ancestor = None
     for cata in catas:
         ancestor = cata.parent
@@ -34,13 +46,33 @@ def check_are_siblings(catas):
         tmp_ancestor = ancestor
 
 def check_des_not_src_children(src_catas, des_cata):
+    '''
+    检查目的结点不是源结点的后代。
+    '''
     for ancestor in des_cata.get_ancestors():
         if ancestor in src_catas:
             raise ParseError()
 
 def check_are_siblings_and_in_root(cata_ids, root):
+    '''
+    检查这些目录是兄弟结点，并且是这个根的后代。
+    '''
     catas = check_exist_catalogues(cata_ids)
     if not catas[0].get_root() == root:
         raise ParseError()
     check_are_siblings(catas)
     return catas
+
+def check_is_ancestor(ancestor, cata):
+    '''
+    检查ancestor是cata的祖先。
+    '''
+    ret = list()
+    all_ancestors = cata.get_ancestors(ascending=True, include_self=True)
+    for an_ancestor in all_ancestors:
+        ret.append(an_ancestor)
+        if ancestor == an_ancestor:
+            break
+    else:
+        raise ParseError()
+    return ret[::-1]
---
+++ backend/storage/views/cross_storage.py
@@ -0,0 +1,74 @@
+'''
+跨仓库相关接口的视图
+'''
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from my_utils.checker import check_are_same
+from my_utils.checker import check_not_none
+from my_utils.checker import check_all_int
+
+from group.checker import check_exist_group
+from group.checker import check_user_in_group
+
+from storage.checker import check_exist_catalogue
+from storage.checker import check_are_siblings_and_in_root
+
+def _check_src_ids(src_ids):
+    check_not_none(src_ids)
+    return check_all_int(src_ids)
+
+def _check_src_and_des(src_root, src_ids, des_root, des_id):
+
+    des_cata = des_root
+    if des_id:
+        des_cata = check_exist_catalogue(des_id)
+    check_are_same(des_cata.get_root(), des_root)
+
+    src_catas = check_are_siblings_and_in_root(src_ids, src_root)
+
+    return src_catas, des_cata
+
+class SaveToMe(APIView):
+    '''
+    save-to-me 相关接口的视图类
+    '''
+    @staticmethod
+    def put(request, group_id, des_id=None):
+        '''
+        将群组仓库内文件保存到个人仓库。
+        '''
+        my_self = request.user.user
+        group = check_exist_group(group_id)
+        check_user_in_group(my_self, group)
+
+        my_root = my_self.storage
+        group_root = group.storage
+        src_ids = _check_src_ids(request.POST.getlist('id', None))
+        src_catas, des_cata = _check_src_and_des(group_root, src_ids, my_root, des_id)
+
+        for cata in src_catas:
+            cata.copy_to(des_cata)
+        return Response()
+
+class UploadToGroup(APIView):
+    '''
+    upload-to-group 相关接口的视图类
+    '''
+    @staticmethod
+    def put(request, group_id, des_id=None):
+        '''
+        将个人仓库内文件保存到群组仓库。
+        '''
+        my_self = request.user.user
+        group = check_exist_group(group_id)
+        check_user_in_group(my_self, group)
+
+        my_root = my_self.storage
+        group_root = group.storage
+        src_ids = _check_src_ids(request.POST.getlist('id', None))
+        src_catas, des_cata = _check_src_and_des(my_root, src_ids, group_root, des_id)
+
+        for cata in src_catas:
+            cata.copy_to(des_cata)
+        return Response()
