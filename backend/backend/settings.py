c6fc02e7071dd3510686f976a1a6dd3484e82dbe
cjc7373 <niuchangcun@163.com>
2019-12-18 13:30:38 +0800

    add cors support

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,12 +25,15 @@ SECRET_KEY = 'ym#m#zs@p#ju$d^duue#zteven#zs7ylb=j3e^3juke^^qjv&h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
