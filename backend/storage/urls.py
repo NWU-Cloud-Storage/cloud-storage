b0cb47653710ce489ddea86c038889685fcdc401
cjc7373 <niuchangcun@163.com>
2020-03-06 11:07:17 +0800

    删除原 group storage, cross storage 代码

--- backend/storage/urls.py
+++ backend/storage/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
 from storage.views.storage import StorageAPI, MyStorageMove, MyStorageCopy, MyStorageFiles
-from storage.views.group_storage import GroupStorage, GroupStorageMove, GroupStorageCopy
-from storage.views.cross_storage import SaveToMe, UploadToGroup
 
 urlpatterns = [
     path("storage/", StorageAPI.as_view()),
@@ -15,13 +13,4 @@ urlpatterns = [
     path("storage/download/<int:src_cata_id>/", MyStorageFiles.as_view()),
     path("group-storage/<int:group_id>/download/<int:src_cata_id>/", MyStorageFiles.as_view()),
 
-    path("group-storage/<int:group_id>/", GroupStorage.as_view()),
-    path("group-storage/<int:group_id>/<int:src_cata_id>/", GroupStorage.as_view()),
-    path("group-storage/move/<int:group_id>/", GroupStorageMove.as_view()),
-    path("group-storage/copy/<int:group_id>/", GroupStorageCopy.as_view()),
-
-    path("save-to-me/<int:group_id>/", SaveToMe.as_view()),
-    path("save-to-me/<int:group_id>/<int:des_id>/", SaveToMe.as_view()),
-    path("upload-to-group/<int:group_id>/", UploadToGroup.as_view()),
-    path("upload-to-group/<int:group_id>/<int:des_id>/", UploadToGroup.as_view()),
 ]
