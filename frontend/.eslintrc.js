c5233e3044a79642e254659ce06826fa077c749f
MiloWang <milowang2048@foxmail.com>
2020-03-18 21:41:19 +0800

    chore: add env BUILD_ENV to detect prodction build

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,9 +8,9 @@ module.exports = {
       'eslint:recommended'
     ],
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-      'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+      'no-console': process.env.BUILD_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.BUILD_ENV === 'production' ? 'error' : 'warn',
+      'no-unused-vars': process.env.BUILD_ENV === 'production' ? 'error' : 'warn'
     },
     parserOptions: {
       parser: 'babel-eslint'
